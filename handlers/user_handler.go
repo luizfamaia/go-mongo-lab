737cb45d67c32a6bbb640e0fc70140df9e1674df
luizfamaia <luizfamaia@gmail.com>
2025-08-13 15:30:25 -0300

    ajuste de mensagem de sucesso

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -31,7 +31,7 @@ func (h *UserHandler) CreateUser(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Usuário inserido com sucesso pra valer a"})
+	c.JSON(http.StatusOK, gin.H{"message": "Usuário inserido com sucesso!!!"})
 }
 
 func (h *UserHandler) UpdateUser(c *gin.Context) {
